755904dd9a488f0670e65f8ad04400af498ae982
niracler <i@niracler.com>
2024-12-05 08:34:48 +0800

    docs: update url list

--- uptime.config.ts
+++ uptime.config.ts
@@ -3,9 +3,9 @@ const pageConfig = {
   title: "Niracler's Status Page",
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
-    { link: 'https://n8n.niracler.com', label: 'n8n' },
-    { link: 'https://autobangumi.niracler.com', label: 'AutoBangumi' },
-    { link: 'https://animashin.niracler.com', label: 'Animashin', highlight: true },
+    { link: 'https://github.com/niracler', label: 'GitHub' },
+    { link: 'https://niracler.com/', label: 'Blog' },
+    { link: 'mailto:i@niracler.com', label: 'Email Me', highlight: true },
   ],
 }
 
@@ -18,47 +18,38 @@ const workerConfig = {
   monitors: [
     // Example HTTP Monitor
     {
-      // `id` should be unique, history will be kept if the `id` remains constant
-      id: 'foo_monitor',
-      // `name` is used at status page and callback message
-      name: 'My API Monitor',
-      // `method` should be a valid HTTP Method
-      method: 'POST',
-      // `target` is a valid URL
-      target: 'https://example.com',
-      // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
-      tooltip: 'This is a tooltip for this monitor',
-      // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
-      statusPageLink: 'https://example.com',
-      // [OPTIONAL] `expectedCodes` is an array of acceptable HTTP response codes, if not specified, default to 2xx
-      expectedCodes: [200],
-      // [OPTIONAL] `timeout` in millisecond, if not specified, default to 10000
-      timeout: 10000,
-      // [OPTIONAL] headers to be sent
-      headers: {
-        'User-Agent': 'Uptimeflare',
-        Authorization: 'Bearer YOUR_TOKEN_HERE',
-      },
-      // [OPTIONAL] body to be sent
-      body: 'Hello, world!',
-      // [OPTIONAL] if specified, the response must contains the keyword to be considered as operational.
-      responseKeyword: 'success',
-      // [OPTIONAL] if specified, the check will run in your specified region,
-      // refer to docs https://github.com/lyc8503/UptimeFlare/wiki/Geo-specific-checks-setup before setting this value
-      checkLocationWorkerRoute: 'https://xxx.example.com',
+      id: 'n8n_monitor',
+      name: 'n8n',
+      method: 'GET',
+      target: 'https://n8n.niracler.com',
+      expectedCodes: [200]
     },
-    // Example TCP Monitor
     {
-      id: 'test_tcp_monitor',
-      name: 'Example TCP Monitor',
-      // `method` should be `TCP_PING` for tcp monitors
-      method: 'TCP_PING',
-      // `target` should be `host:port` for tcp monitors
-      target: '1.2.3.4:22',
-      tooltip: 'My production server SSH',
-      statusPageLink: 'https://example.com',
-      timeout: 5000,
+      id: 'autobangumi_monitor',
+      name: 'AutoBangumi',
+      method: 'GET',
+      target: 'https://autobangumi.niracler.com',
+      expectedCodes: [200]
     },
+    {
+      id: 'animashin_monitor',
+      name: 'Animashin',
+      method: 'GET',
+      target: 'https://animashin.niracler.com',
+      expectedCodes: [200]
+    }
+    // Example TCP Monitor
+    // {
+    //   id: 'test_tcp_monitor',
+    //   name: 'Example TCP Monitor',
+    //   // `method` should be `TCP_PING` for tcp monitors
+    //   method: 'TCP_PING',
+    //   // `target` should be `host:port` for tcp monitors
+    //   target: '1.2.3.4:22',
+    //   tooltip: 'My production server SSH',
+    //   statusPageLink: 'https://example.com',
+    //   timeout: 5000,
+    // },
   ],
   notification: {
     // [Optional] apprise API server URL
